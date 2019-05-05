abeaf3bf0cdf5f2c48e6be02e30a0ec200cb9965
Atakan Savas <atakansavas@Atakans-MacBook-Pro.local>
2019-05-05 23:25:30 +0300

    hide configs

--- components/Utility.tsx
+++ components/Utility.tsx
@@ -3,14 +3,7 @@ import { Alert } from "react-native";
 
 export default class Utility {
   static initFirebase() {
-    firebase.initializeApp({
-      apiKey: "AIzaSyDhCt1hM6CLt3Fsklicq7yqi8IZItnCROE",
-      authDomain: "reactnative-auth-test-3480d.firebaseapp.com",
-      databaseURL: "https://reactnative-auth-test-3480d.firebaseio.com",
-      projectId: "reactnative-auth-test-3480d",
-      storageBucket: "reactnative-auth-test-3480d.appspot.com",
-      messagingSenderId: "146226404361"
-    });
+
   }
 
   static showAlert(title: string = "FirebaseApp", description: string) {
